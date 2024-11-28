9909f2bd46f6dea9093117f98c118a10aa5154f3
testacnarendra <testacnarendra@gmail.com>
2024-11-28 16:09:14 +0530

    changes in file

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -4,12 +4,15 @@
 sudo apt-get update
 sudo apt install -y python3-pip python3-venv nginx
 
-# Navigate to the project directory (update path as needed)
-cd /home/ubuntu/
+
 
 # Create a virtual environment and install dependencies
 python3 -m venv env
 source env/bin/activate
+
+# Navigate to the project directory (update path as needed)
+cd /home/ubuntu/blogprojectdrf
+
 pip install -r requirements.txt || echo "requirements.txt not found or pip failed to execute"
 
 echo "Instance cleanup and setup complete."
