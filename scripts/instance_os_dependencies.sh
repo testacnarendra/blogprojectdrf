9a0c9bac3456dfa4ae3955a6fe9ba430f5f1f4fa
testacnarendra <testacnarendra@gmail.com>
2024-12-04 13:57:01 +0530

    changes

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # Update the package list and install system dependencies
-sudo apt-get update
+sudo apt-get update -y
 sudo apt install python3-venv python3-dev libpq-dev postgresql postgresql-contrib nginx curl
-sudo apt-get install python3-pip
+sudo apt-get install python3-pip -y
 pip install --upgrade pip
 
 # Create a virtual environment and install dependencies
 cd /home/ubuntu/
 python3 -m venv env
 source env/bin/activate
-pip install django gunicorn psycopg2-binary
+pip3 install django gunicorn psycopg2-binary
 
 # Navigate to the project directory (update path as needed)
 cd /home/ubuntu/blogprojectdrf/
-pip3 install -r requirements.txt || echo "requirements.txt not found or pip failed to execute"
+pip3 install -r requirements.txt
 
 echo "Instance cleanup and setup complete."
