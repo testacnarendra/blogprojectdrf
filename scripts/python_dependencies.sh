56307d62a87f3d34d493af14dc5c1dfdcc0f2955
testacnarendra <testacnarendra@gmail.com>
2024-12-04 14:31:25 +0530

    comment al

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -2,6 +2,6 @@
 
 # virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
-sudo apt-get install python3-pip -y
-pip install --upgrade pip
-pip3 install -r /home/ubuntu/blogprojectdrf/requirements.txt
+# sudo apt-get install python3-pip -y
+# pip install --upgrade pip
+# pip3 install -r /home/ubuntu/blogprojectdrf/requirements.txt
